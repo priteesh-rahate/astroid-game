3bbb997f72e0d77ccbf2a77771df454d8289b572
priteesh-rahate <priteeshrahate04@gmail.com>
2026-07-01 11:27:50 +0000

    Add Player class with draw method

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import pygame
 from logger import log_state
 from constants import *
+from player import *
 def main():
    pygame.init()
    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
    print(f"Screen width: {SCREEN_WIDTH}")
    print(f"Screen height: {SCREEN_HEIGHT}")
    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   clock = pygame.time.Clock()
+   dt = 0.0
+   player = Player(
+      SCREEN_WIDTH / 2,
+      SCREEN_HEIGHT / 2
+      )
    while True :
       log_state()
       for event in pygame.event.get():
           if event.type == pygame.QUIT:
             return
       screen.fill("black")
+      player.draw(screen)
       pygame.display.flip()
+      dt = clock.tick(60) / 1000
+      
+      
 
 
 
